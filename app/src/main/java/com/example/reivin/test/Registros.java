436da3355bc1d1169fb3b15ca4d13ab1fbb8a1c9
Reivin <kervheros@gmail.com>
2017-12-20 08:22:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/reivin/test/Registros.java
@@ -0,0 +1,13 @@
+package com.example.reivin.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros);
+    }
+}
